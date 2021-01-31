df0d9963f72184614a4609595b719a0c27e2109b
Anders Koo Furulund <anderskoo@live.no>
2021-01-31 19:04:43 +0100

    switched to HashRouter in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter>
+  // <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
+  // </BrowserRouter>,
   document.getElementById("root")
 );
 

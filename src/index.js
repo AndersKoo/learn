65abe90bbbebfc96362cd35b7ee3eceb37d8fb27
Anders Koo Furulund <anderskoo@live.no>
2021-01-31 20:50:43 +0100

    router update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <BrowserRouter>
-    {/* <HashRouter> */}
     <App />
-    {/* </HashRouter>, */}
   </BrowserRouter>,
   document.getElementById("root")
 );
